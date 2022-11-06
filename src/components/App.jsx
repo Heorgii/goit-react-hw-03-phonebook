5dfb55bb0048337a787d09556bee5a8e85bb06c9
Heorgii <rushchakheorgii@gmail.com>
2022-11-06 18:54:14 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,17 @@ import { nanoid } from 'nanoid'
 import React, { Component } from 'react';
 import Filter from './Filter/Filter';
 import ContactForm from './ContactForm/ContactForm';
-// import ContactList from './ContactList/ContactList';
 import ContactList from './ContactList/ContactList';
+import Notification from './Notification/Notification';
 import css from './App.module.css';
 
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
     // name: '',
@@ -72,8 +72,23 @@ class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   }
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
-    const { filter } = this.state;
+    const { contacts, filter } = this.state;
     const getContacts = this.getContacts();
     return (
       <div>
@@ -81,10 +96,19 @@ class App extends Component {
         <ContactForm onSubmit={this.addContact} />
 
         <h2 className={css.contact_title}>Contacts</h2>
-        <Filter value={filter} onChange={this.filterChange} />
-        <ContactList
-          contacts={getContacts}
-          onDeleteContact={this.deleteContact} />
+
+        {contacts.length > 0 ? (
+          <>
+            <Filter value={filter} onChange={this.filterChange} />
+            <ContactList
+              contacts={getContacts}
+              onDeleteContact={this.deleteContact} />
+          </>
+        ) : (
+          <Notification message="Contact list is empty" />
+        )}
+
+
       </div>
     );
   }
